126364a87784bc2181eb6206cd68896f8a37b383
ShouvikSQA <shouvik9292@gmail.com>
2026-05-02 21:52:41 +0600

    My first commit

---
+++ report.js
@@ -0,0 +1,24 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+   
+    collection:require('./DMoney_API_Collection_B18.postman_collection.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+     delayRequest: 5000, 
+         envVar: [
+        {
+            key: "gmailToken",
+            value:  process.env.token
+        }
+    ],
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html',
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
